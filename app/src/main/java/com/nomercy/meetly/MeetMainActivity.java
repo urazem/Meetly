2b0f2749584c69b497405f4719399f7c0cff3b2e
Emine_ <urazalieva.e.i.2.16@gmail.com>
2018-11-20 18:15:42 +0300

    Changed xml files

---
+++ app/src/main/java/com/nomercy/meetly/MeetMainActivity.java
@@ -0,0 +1,13 @@
+package com.nomercy.meetly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeetMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meet_main);
+    }
+}
